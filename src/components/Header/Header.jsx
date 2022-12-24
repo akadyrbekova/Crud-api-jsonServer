0b72974759b6255f59ba48e56584f0b064a78502
akadyrbekova <adilya.kadyrbekova.00@gmail.com>
2022-12-24 15:38:35 +0300

    add navbar's link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand>Navbar</Navbar.Brand>
+        <Nav className=" me-auto">
+          <Link to="/">
+            <Nav.Item>Home</Nav.Item>
+          </Link>
+          <Link to="/list">
+            <Nav.Item>Product list</Nav.Item>
+          </Link>
+          <Link to="/add">
+            <Nav.Item>Add product</Nav.Item>
+          </Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Header;
